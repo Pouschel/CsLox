31ce1b0fab9d013fbc5d6551d77a72af344d9b93
ts <>
2021-12-19 17:55:08 +0100

    14.5.0

---
+++ CsLox/Chunk.cs
@@ -0,0 +1,41 @@
+﻿namespace CsLox;
+
+
+enum OpCode : byte
+{
+	OP_RETURN
+}
+
+class Chunk : List<byte>
+{
+	public void write(OpCode oc)
+	{
+		this.Add((byte)oc);
+	}
+
+	public void disassemble(string name, TextWriter? tw = null)
+	{
+		tw ??= Console.Out;
+		tw.WriteLine($"== {name} ==");
+
+		for (int offset = 0; offset < Count;)
+		{
+			offset = disassembleInstruction(offset, tw);
+		}
+	}
+
+	int disassembleInstruction(int offset, TextWriter tw)
+	{
+		tw.Write($"{offset:0000} ");
+		var instruction = (OpCode)this[offset];
+		switch (instruction)
+		{
+			case OP_RETURN:
+				tw.WriteLine(instruction);
+				return offset + 1;
+			default:
+				tw.WriteLine($"Unknown opcode {instruction}");
+				return offset + 1;
+		}
+	}
+}
---
+++ CsLox/Program.cs
@@ -0,0 +1,24 @@
+﻿
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using CsLox;
+global using static CsLox.OpCode;
+
+using System.Runtime.InteropServices;
+
+class Program
+{
+
+	static void Main()
+	{
+		Chunk chunk = new Chunk();
+		chunk.write(OP_RETURN);
+		chunk.disassemble("test");
+
+		Console.ReadLine();
+	}
+
+}
