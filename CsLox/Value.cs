c854a4e7bd7e885d1b7ec102bc79f50f23878ce3
ts <>
2021-12-20 07:50:57 +0100

    19.1 Values and Objects

--- CsLox/Value.cs
+++ CsLox/Value.cs
@@ -8,20 +8,30 @@ enum ValueType
 	VAL_BOOL,
 	VAL_NIL,
 	VAL_NUMBER,
+	VAL_OBJ
 }
 
 
 struct Value
 {
+	static object DummyObject = new object();
+	
 	public ValueType type;
 	public double dValue;
-	public object? oValue;
+	public object oValue;
 
-	public Value(ValueType type, double dValue, object? oValue = null)
+	public Value(ValueType type, object oValue)
+	{
+		this.type = type;
+		this.dValue = double.NaN;
+		this.oValue = oValue;
+	}
+
+	public Value(ValueType type, double dValue)
 	{
 		this.type = type;
 		this.dValue = dValue;
-		this.oValue = oValue;
+		this.oValue = DummyObject;
 	}
 
 	public override string ToString()
@@ -39,17 +49,18 @@ struct Value
 static class ValueStatics
 {
 	public static Value BOOL_VAL(bool value) => new(VAL_BOOL, value ? 1 : 0);
-
 	public static readonly Value NIL_VAL = new(VAL_NIL, 0);
-
 	public static Value NUMBER_VAL(double value) => new(ValueType.VAL_NUMBER, value);
+	public static Value OBJ_VAL(object value) => new(ValueType.VAL_OBJ, value);
 
+	public static object AS_OBJ(Value value) => value.oValue; 
 	public static bool AS_BOOL( Value value) => value.dValue != 0;
 	public static double AS_NUMBER( Value value) => value.dValue;
 
 	public static bool IS_BOOL( Value value) => value.type == VAL_BOOL;
 	public static bool IS_NIL( Value value) => value.type == VAL_NIL;
 	public static bool IS_NUMBER(Value value) => value.type == VAL_NUMBER;
+	public static bool IS_OBJ(Value value) => value.type == VAL_OBJ;
 	public static bool isFalsey(Value value) => IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
 	public static bool valuesEqual(Value a, Value b)
 	{
